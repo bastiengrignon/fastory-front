d370783099c6c49334bb91610448f09bfc63f231
bastiengrignon <basgrignon@gmail.com>
2021-10-07 01:08:36 +0200

    Add Axios library

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+
+(() => {
+    axios.defaults.baseURL = process.env.REACT_APP_API_URL
+})()
 
 ReactDOM.render(
     <React.StrictMode>

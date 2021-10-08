771e37d4694dd893003383c872ea9952525ca7bf
bastiengrignon <basgrignon@gmail.com>
2021-10-09 00:26:54 +0200

    Add select filter

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,23 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { Types } from '../../interfaces'
+import { FilterService } from '../../service/filter.service'
 import Card from '../Card'
 import Input from '../Input'
 import List from '../List'
 
 const App = () => {
     const [results, setResults] = useState<Types[]>([])
+    const [filteredResults, setFilteredResults] = useState<Types[]>(results)
     const [keywordToSend, setKeywordToSend] = useState<string>("")
 
     const sendSearchRequest = async (keyword: string): Promise<void> => {
         const result = await axios.get('/', { params: { keyword } }).then(r => r.data)
         setResults(result)
+        setFilteredResults(result)
     }
+
+    const filterCards = (event: ChangeEvent<HTMLSelectElement>) => setFilteredResults(results.filter(type => FilterService.getTypeFromObject(type, event.target.value)))
 
     return (
         <div className="space-y-5 m-10">
@@ -22,14 +27,25 @@ const App = () => {
                         onClick={ () => sendSearchRequest(keywordToSend) }>
                     Search
                 </button>
+                <select
+                    className="rounded-md outline-none border-2 border-transparent focus:border-blue-400 bg-gray-400 hover:bg-white focus:bg-white px-2"
+                    onChange={ filterCards }>
+                    <option value="none">None</option>
+                    <option value="people">People</option>
+                    <option value="films">Films</option>
+                    <option value="vehicles">Vehicles</option>
+                    <option value="starships">Starships</option>
+                    <option value="species">Species</option>
+                    <option value="planets">Planets</option>
+                </select>
             </div>
             <div className="grid grid-cols-6 gap-5">
                 <div className="col-span-2">
-                    <List data={ results }/>
+                    <List data={ filteredResults }/>
                 </div>
                 <div className="col-span-4 grid grid-cols-3 gap-2">
                     {
-                        results.map((result, key) => <Card key={ key } data={ result }/>)
+                        filteredResults.map((result, key) => <Card key={ key } data={ result }/>)
                     }
                 </div>
             </div>
